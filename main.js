a2bafb25d4bc8de4e336608628526e61861ea494
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-05-08 21:05:11 -0300

    mas metodos

--- main.js
+++ main.js
@@ -5,11 +5,10 @@
 })
  */
 
-
-$(function(){
-    //$("p").css({'background-color': "red"});
-    //$("#segundo").css({'background-color': "green"});
-/*     $(".primero").dblclick(function(){
+$(function () {
+  //$("p").css({'background-color': "red"});
+  //$("#segundo").css({'background-color': "green"});
+  /*     $(".primero").dblclick(function(){
         $("#segundo").hide();        
          
     })
@@ -19,20 +18,30 @@ $(function(){
          
     }) */
 
-    $('.primero').animate({width: '600px'})
-    $(".primero").mouseenter(function(){
-        $("#segundo").fadeOut();        
-         
-    })
-    $(".primero").mouseleave(function(){
-        $('#segundo').fadeIn();
-        
-         
-    })
-    $('#btn-hide').click(function(){
-        alert($('#segundo').attr('title'));
+  $(".primero").animate({ width: "600px" });
+  $(".primero").mouseenter(function () {
+    $("#segundo").fadeOut();
+  });
+  $(".primero").mouseleave(function () {
+    $("#segundo").fadeIn();
+  });
+  $("#btn-hide").click(function () {
+    // alert($('#segundo').attr('title'));
+    // ($('#segundo').text('hola'))
+    //($('#segundo').append('hola <strong>color negrita</strong> a todos'))
+    //($('#segundo').prepend('hola <strong>color negrita</strong> a todos'))
+    // ($('#segundo').after('<p>Hola <strong>color negrita</strong> a todos</p>'))
+    // ($('#segundo').before('<p>Hola <strong>color negrita</strong> a todos</p>'))
+    // ($('#segundo').remove('<p>Hola <strong>color negrita</strong> a todos</p>'))
+    //($('#btn-show').empty())
+    //$("h1").addClass("blue");
+   // $("h1").addClass("blue");
+    $("h1").toggleClass("blue");
 
+  });
+/*   $("#btn-show").click(function () {
 
-    }
-    )
-})
+    $("h1").removeClass("blue");
+
+  }); */
+});
